97a909bcbc7e5e43bbe2c309e54a89833de8ed4e
Youkyoung Park <kyoungah@kyoungah.com>
2018-05-02 15:00:47 +0900

    실행 파일 제거

--- algorithms-questions/checkPalindrome.c
+++ algorithms-questions/checkPalindrome.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdbool.h>
 /**
  * 안녕하세요, 매일프로그래밍 이번주 문제입니다.
  * 정수(int)가 주어지면, 팰린드롬(palindrome)인지 알아내시오. 
@@ -20,50 +20,91 @@
  * Output: True
 */
 
-int getpositiveNum (int num) {
-  if (num < 0) {
-    num *= -1;
+/** 
+ * 매일 프로그래밍 풀이
+ * 이제 주어진 숫자를 전반과 후반으로 나누어 줍니다. 
+ * 예를 들면 123321이 주어지면, 123이 전반이고 321이 후반입니다. 
+ * 전반(123)이 거꾸로된 후반(123)과 값이 같다면 팰린드롬이겠죠. 
+ * 정수를 전반과 거꾸로 된 후반으로 나누려면 while loop을 써서 정수의 일의 자리수를 뽑아낸후 정수를 10으로 나누면 정수의 각 숫자를 얻어낼수 있습니다.
+ * 123321 -> 12332, 1 -> 1233, 2 -> 123, 3.
+ * 이제 거꾸로 된 후반은 10으로 곱한뒤 뽑아낸 일의 자리 숫자를 더하면 됩니다.
+ * 0 + 1 -> 10 + 2 -> 120 + 3 = 123.
+ * ﻿이 방법을 거꾸로된 후반이 전반보다 크거나 같을때까지 반복하면 됩니다.
+ * 만약 주어진 숫자의 길이가 홀수라면, 거꾸로된 후반에서 1의 자리수를 없애주면 됩니다.
+ * 2321 -> 전반(12), 거꾸로된 후반(123)
+*/
+int getCheckPalindrome2 (int num) {
+  bool isPalindrome = false;
+
+  if (num < 0 || (num % 10 == 0 && num != 0)) {
+    return isPalindrome;
   }
 
-  return num;
-}
+  int num2 = 0;
 
-char* getCheckPalindrome (int* numList, int length) {
-  char* isPalindrome = "True";
-
-  for (int i = 0; i < length; i++) {
-        printf("%d %d\n", numList[i], numList[(length - 1) -i]);
-    if (numList[i] != numList[(length - 1) -i]) {
-      isPalindrome = "False";
-    }
+  while (num > num2) {
+    num2 = (num2 * 10) + (num % 10);
+    num = num / 10;
   }
 
+  if (num == num2 || num == (num2/10)) {
+    isPalindrome = true;
+  }
 
   return isPalindrome;
 }
 
-int main () {
-  int num = 0, positiveNum = 0, length = 1;
-  int* numList;
+/** 
+ * 내 풀이
+ * num 을 받으면 각각 자리수를 잘라 배열에 저장해서 배열의 앞부분과 뒷부분을 하나씩 비교해서 같은지 확인 
+*/
+int getCheckPalindrome (int num, int* numList, int length) {
+  bool isPalindrome = true;
 
-  scanf("%d", &num);
+  if (num < 0) {
+    isPalindrome = false;
 
-  positiveNum = getpositiveNum(num);
+    return isPalindrome;
+  }
+
+  for (int i = 0; i < length; i++) {
+    if (numList[i] != numList[(length - 1) -i]) {
+      isPalindrome = false;
+    }
+  }
+
+  return isPalindrome;
+}
+
+void getNumList (int num, int* numList, int length) {
   numList = (int *)malloc(sizeof(int) * length);
 
-  for (int i = 0; positiveNum > 0; i++) {
-    numList[i] = (int) positiveNum % 10;
-    positiveNum = positiveNum / 10;
-
-    if (i == 0 && num < 0) {
-      numList[i] *= -1;
-    }
-
-    if (positiveNum > 0) {
+  for (int i = 0; num > 0; i++) {
+    numList[i] = (int) num % 10;
+    num = num / 10;
+    
+    if (num > 0) {
       length += 1;
       numList = realloc(numList, sizeof(int) * length);
     }
   }
+}
 
-  printf("%s\n", getCheckPalindrome(numList, length));
+int main () {
+  int num = 0, length = 1;
+  int* numList;
+
+  scanf("%d", &num);
+
+  // if (getCheckPalindrome(num, numList, length) == true) {
+  //   printf("True\n");
+  // } else {
+  //   printf("False\n");
+  // }
+
+  if (getCheckPalindrome2(num) == true) {
+    printf("True2\n");
+  } else {
+    printf("False2\n");
+  }
 }
